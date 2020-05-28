27207257354a8382f3889269b26aa74fcacece20
Lukas Beiske <lukas.beiske@tu-dortmund.de>
2020-05-28 12:06:21 +0200

    Update disp config - not working

--- file_convert.py
+++ file_convert.py
@@ -12,14 +12,14 @@ def main(infile, outfile, inkey):
 
     # simulations
     if 'mc_az' in df.columns:
-        df['pointing_azimuth'] = np.rad2deg(df.mc_az_tel)
-        df['pointing_zenith'] = 90 - np.rad2deg(df.mc_alt_tel)
-        df['source_azimuth'] = np.rad2deg(df.mc_az)
-        df['source_zenith'] = 90 - np.rad2deg(df.mc_alt)
+        #df['pointing_azimuth'] = np.rad2deg(df.mc_az_tel)
+        df['pointing_zenith'] = np.pi/2 - df.mc_alt_tel
+        #df['source_azimuth'] = np.rad2deg(df.mc_az)
+        df['source_zenith'] = np.pi/2 - df.mc_alt
     # observations
     else:
-        df['pointing_azimuth'] = np.rad2deg(df.az_tel)
-        df['pointing_zenith'] = 90 - np.rad2deg(df.alt_tel)
+        #df['pointing_azimuth'] = np.rad2deg(df.az_tel)
+        df['pointing_zenith'] = np.pi/2 - df.alt_tel
     
     #df['psi_deg'] = np.rad2deg(df.psi)
     df['focal_length'] = 28
