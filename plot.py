2ba750b89dbc74045662a88c90534940f6b497c0
Lukas Beiske <lukas.beiske@tu-dortmund.de>
2020-05-29 12:33:07 +0200

    Update config and plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from fact.io import read_h5py
+from fact.analysis.statistics import li_ma_significance
 import pandas
 
 import matplotlib
@@ -16,14 +17,29 @@ def dist_to_camera(on_df, off_df=None, ax=None, range=[0,0.12]):
     dist_on = on_df_selected.source_x_prediction**2 + on_df_selected.source_y_prediction**2
 
     if isinstance(off_df, pandas.core.frame.DataFrame):
+        time_on = on_df.dragon_time
+        total_time_on = time_on.max() - time_on.min()
+        print(f'Total time ON = {total_time_on}')
+        time_off = off_df.dragon_time
+        total_time_off = time_off.max() - time_off.min()
+        print(f'Total time OFF = {total_time_off}')
+        
+        scaling = total_time_off/total_time_on
         off_df_selected = off_df.query('gammaness > 0.5')
         dist_off = off_df_selected.source_x_prediction**2 + off_df_selected.source_y_prediction**2
-        ax.hist(dist_off, bins=100, range=range, histtype='step', label='OFF', weights=np.full_like(dist_off, 0.9))
+        ax.hist(dist_off, bins=100, range=range, histtype='step', label='OFF', weights=np.full_like(dist_off, scaling))
 
     ax.hist(dist_on, bins=100, range=range, histtype='step', label='ON')
     ax.set_xlabel(r'$distance^2_{\mathrm{camera-center}} \,\, / \,\, \mathrm{m}^2$')
     ax.legend()
     ax.figure.tight_layout()
+
+    if isinstance(off_df, pandas.core.frame.DataFrame):
+        cut = 0.005
+        n_off = np.count_nonzero(dist_off < cut)
+        n_on = np.count_nonzero(dist_on < cut)
+        li_ma = li_ma_significance(n_on, n_off, scaling)
+        print(f'N_off = {n_off} \nN_on = {n_on} \nLi-Ma significance = {li_ma}')
 
     return ax
 
